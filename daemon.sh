909161ddb37f8ba2d527b0af0f8255a94b15ae74
Paul Marks <paul@pmarks.net>
2022-11-09 19:33:53 -0500

    Use a proper tempfile.

--- daemon.sh
+++ daemon.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 dir="$(dirname -- "$(realpath -- "$0")")"
+h="$(mktemp --tmpdir exhale.XXXX)"
 screen -ls exhale && echo "Daemon already running" && exit 1
 
 # This script runs exhale.py in an interactive screen daemon.
 # You can customize these command line flags:
-echo "cd $dir; ./exhale.py co2 --zdevice=/dev/ttyS0 --scd30_i2c=6" >/tmp/hist
+echo "cd $dir; ./exhale.py co2 --zdevice=/dev/ttyS0 --scd30_i2c=6" >"$h"
 
-HISTFILE=/tmp/hist screen -dmS exhale -h 10000 bash --init-file /tmp/hist && echo "Daemon started"
+HISTFILE="$h" screen -dmS exhale -h 10000 bash --init-file "$h" \
+    && echo "Daemon started"
